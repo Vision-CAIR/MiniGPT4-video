3c6b68885766d3ed817310960f7c400b6907f22d
KerolosAtef <keroatef295@gmail.com>
2024-04-02 20:06:57 +0300

    run_on_mutiple_machines

--- jobs_video/eval/submit_job.py
+++ jobs_video/eval/submit_job.py
@@ -3,10 +3,17 @@ import shutil
 import sys
 
 start=0
-end=7650
-step=1000
+end=7800
+step=800
 
+# Mistral
 for i in range(start,end,step):
     cmd=f'sbatch ./mistral_evalualtion.sh {i} {i+step}'
     # print(cmd)
     os.system(cmd)
+  
+# Llama 2  
+# for i in range(start,end,step):
+#     cmd=f'sbatch ./llama2_evalualtion.sh {i} {i+step}'
+#     # print(cmd)
+#     os.system(cmd)
