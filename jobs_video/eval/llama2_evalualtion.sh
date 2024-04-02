6f60f539df5d86776272b42ae11cd3e36bc8f0cc
KerolosAtef <keroatef295@gmail.com>
2024-04-02 19:51:42 +0300

    update_eval_scripts

--- jobs_video/eval/llama2_evalualtion.sh
+++ jobs_video/eval/llama2_evalualtion.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --mail-user=kirolos.ataallah@kaust.edu.sa
-#SBATCH --mail-type=ALL
-#SBATCH --job-name=llama2_stage_3_best%j
-#SBATCH --output=llama2_stage_3_best%j.out
-#SBATCH --error=llama2_stage_3_best%j.err
+#SBATCH --job-name=llama2_best%j
+#SBATCH --output=llama2_best%j.out
+#SBATCH --error=llama2_best%j.err
 #SBATCH --time=0-23:00:00
 #SBATCH --mem=100G
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 ## run the application:
-NAME="llama2_stage_3_best" # Name of the experiment
-DATASET="msvd" # available datasets: tvqa, msrvtt, msvd, activitynet,tgif ,video_chatgpt_generic,video_chatgpt_temporal,video_chatgpt_consistency
+NAME="llama2_best" # Name of the experiment
+DATASET="tvqa" # available datasets: tvqa, msrvtt, msvd, activitynet,tgif ,video_chatgpt_generic,video_chatgpt_temporal,video_chatgpt_consistency
 BATCH_SIZE=8 
-CKPT_PATH="checkpoints/video_llama_checkpoint_best.pth" # path to the checkpoint
+CKPT_PATH="checkpoints/video_llama_checkpoint_last.pth" # path to the checkpoint
 cfg_path="test_configs/llama2_test_config.yaml" # path to the config file
-cd ../../
+# # if the number of samples are large you can specify the start and end index to evaluate on several machines
+# pass the start and end index as arguments
+start=$1 # start index
+end=$2 # end index
+# if start and end are not provided, then use the whole dataset
+if [ -z "$START" ]
+then
+      START=0
+fi
+if [ -z "$END" ]
+then
+      END=10000000
+fi
+echo "Start: $START"
+echo "End: $END"
 
+cd ../../
 # without subtitles
-python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path
+python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path --start $start --end $end
 
 # with subtitles
-# python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path --add_subtitles
+# python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path --add_subtitles --start $start --end $end
--- jobs_video/eval/mistral_evalualtion.sh
+++ jobs_video/eval/mistral_evalualtion.sh
@@ -2,21 +2,38 @@
 #SBATCH --partition=batch
 #SBATCH --mail-user=kirolos.ataallah@kaust.edu.sa
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=mistral_videoInstruction_video_chatgpt_generic_no_sub%j
-#SBATCH --output=mistral_videoInstruction_best_video_chatgpt_generic_no_sub%j.out
-#SBATCH --error=mistral_videoInstruction_best_video_chatgpt_generic_no_sub%j.err
+#SBATCH --job-name=mistral_best%j
+#SBATCH --output=mistral_best%j.out
+#SBATCH --error=mistral_best%j.err
 #SBATCH --time=0-23:00:00
 #SBATCH --mem=100G
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 ## run the application:
-NAME="mistral_videoInstruction_best" # Name of the experiment
-DATASET="video_chatgpt_generic" # available datasets: tvqa, msrvtt, msvd, activitynet,tgif,video_chatgpt_generic,video_chatgpt_temporal,video_chatgpt_consistency
-BATCH_SIZE=2 # batch size A100 by using subtiles is 2 and without subtitles is 4 
+NAME="mistral_best" # Name of the experiment
+DATASET="tvqa" # available datasets: tvqa, msrvtt, msvd, activitynet,tgif,video_chatgpt_generic,video_chatgpt_temporal,video_chatgpt_consistency
+BATCH_SIZE=4 # batch size for A100 by using subtiles is 2 and without subtitles is 4 
 CKPT_PATH="checkpoints/video_mistral_checkpoint_best.pth" # path to the checkpoint
 cfg_path="test_configs/mistral_test_config.yaml" # path to the config file
 # # if the number of samples are large you can specify the start and end index to evaluate on several machines
-# start=$1 # start index
-# end=$2 # end index
+# pass the start and end index as arguments
+start=$1 # start index
+end=$2 # end index
+# if start and end are not provided, then use the whole dataset
+if [ -z "$START" ]
+then
+      START=0
+fi
+if [ -z "$END" ]
+then
+      END=10000000
+fi
+echo "Start: $START"
+echo "End: $END"
+
 cd ../../
-python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path 
+# without subtitles
+python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path  --start $start --end $end
+
+# with subtitles
+# python eval_video.py --dataset $DATASET --batch_size $BATCH_SIZE --name $NAME --ckpt $CKPT_PATH  --cfg-path=$cfg_path --add_subtitles --start $start --end $end
