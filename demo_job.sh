c067e5cc509c7f5ed411496e7023873e98bfbf9a
KerolosAtef <keroatef295@gmail.com>
2024-04-06 14:12:00 +0300

    update_demo_image

---
+++ demo_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=video_demo_llama2
+#SBATCH --output=video_demo_llama2.out
+#SBATCH --error=video_demo_llama2.err
+#SBATCH --time=0-72:30:00
+#SBATCH --mem=100G
+#SBATCH --gres=gpu:a100:1
+#SBATCH --nodes=1
+
+#  Choose the model to test 
+# Mistral 
+# ckpt="checkpoints/video_mistral_checkpoint_last.pth"
+# config="test_configs/mistral_test_config.yaml"
+
+# Llama2
+ckpt="checkpoints/video_llama_checkpoint_last.pth"
+config="test_configs/llama2_test_config.yaml"
+
+
+/ibex/ai/home/ataallka/miniforge-pypy3/envs/gradio_test/bin/python3.9 -u  minigpt4_video_demo.py --cfg-path $config --ckpt $ckpt
