4b8321a6235b28030fed07145247b86f015ab6b0
KerolosAtef <keroatef295@gmail.com>
2024-04-23 09:01:37 +0300

    fix_save_checkpoint

--- minigpt4/runners/runner_base.py
+++ minigpt4/runners/runner_base.py
@@ -428,10 +428,10 @@ class RunnerBase:
             #                 wandb.log({"epoch": cur_epoch, "GPT4_Accuracy": val_log['agg_metrics']})
             #                 print("Validation finished")
 
-            else:
-                # if no validation split is provided, we just save the checkpoint at the end of each epoch.
-                if not self.evaluate_only:
-                    self._save_checkpoint(cur_epoch, is_best=False)
+            # else:
+            # if no validation split is provided, we just save the checkpoint at the end of each epoch.
+            if not self.evaluate_only:
+                self._save_checkpoint(cur_epoch, is_best=False)
 
             if self.evaluate_only:
                 break
